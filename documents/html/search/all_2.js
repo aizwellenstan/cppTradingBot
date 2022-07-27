27bdc21131f21940ebe2895cde5f79f0be9d66d9
aizwellenstan <aizwellenstan@users.noreply.github.com>
2022-07-27 04:07:40 +0000

    Update generated documentation

--- documents/html/search/all_2.js
+++ documents/html/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['gettestingtf_8',['getTestingTF',['../namespace_trading_bot.html#a6de8a8e018a90a1c46eddb9643096bed',1,'TradingBot']]],
+  ['gettestingtf_8',['getTestingTF',['../namespace_trading_bot.html#afbfc4531149e5689a8c38d5c72576a8a',1,'TradingBot']]],
   ['gettestingtf_2ecpp_9',['getTestingTF.cpp',['../get_testing_t_f_8cpp.html',1,'']]],
   ['gettestingtf_2eh_10',['getTestingTF.h',['../get_testing_t_f_8h.html',1,'']]]
 ];
